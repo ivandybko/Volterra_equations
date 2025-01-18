f95f1a1d407044aed5ad1f0e2f0a6dd3615f3a1e
Ivan Dybko <ivandybfk12@gmail.com>
2025-01-18 23:21:35 +0300

    Delete Source code/Example1.m

--- Source code/Example1.m
+++
@@ -1,6 +0,0 @@
-Ker = @(x, t) ones(size(x)); % Определение функции ядра K(x, t)
-func = @(x) 1 + (x.^2)/2; % Определение функции f(x)
-[x,y] = Method_of_Averaging_Functional_Corrections(Ker, func, 0, 20, 0.005, 1000,0.000001);
-dlmwrite('/Users/ivandybko/Projects/Term paper Volterra equation/Data/Examples/example_ex1_MoAFC_0_20.txt', [x', y'], 'delimiter', '\t', 'precision', '%.16f');
-[x,y] = Quadrature_Method(Ker, func, 0, 20, 20000);
-dlmwrite('/Users/ivandybko/Projects/Term paper Volterra equation/Data/Examples/example_ex1_Quadrature_0_20.txt', [x', y'], 'delimiter', '\t', 'precision', '%.16f');
